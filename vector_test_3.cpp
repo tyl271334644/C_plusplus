d6eb57cb3af6dcd95c6d56566a48c77dfd40a8e1
tyl271334644 <271334644@qq.com>
2019-09-23 16:32:56 +0800

    vector_test_3

---
+++ vector_test_3.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <stdlib.h>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+
+
+	/*int a[] = {1,23,2,3,5,6,10,20,3,2,1};
+	vector<int> v(a,a+sizeof(a)/sizeof(int));
+	cout << "初始数据：";
+	for (auto x:v)
+	{
+		cout<< x << " ";
+	}
+	cout << endl;
+	cout << "从小到大排序：";
+	sort(v.begin(), v.end());
+	for (auto x : v)
+	{
+		cout << x << " ";
+	}
+	
+	cout << endl;
+	sort(v.rbegin(), v.rend());
+	cout << "从大到小排序：";
+	for (auto x : v)
+	{
+		cout << x << " ";
+	}
+	cout << endl;*/
+	system("pause");
+	return 0;
+}
